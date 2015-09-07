c27380b339a9e8578395a057c964caf5c2163a2b
unknown <tim@timrich.org>
2015-09-07 12:37:56 +0300

    Added getproduct.php php code that connect's to mysql and get from database a table content

---
+++ Task 2/app/data/getproduct.php
@@ -0,0 +1,31 @@
+<?php
+
+$con = mysqli_connect('localhost','root','','task2');
+ 
+if (!$con)
+{
+  die('Could not connect: ' . mysql_error());
+}
+ 
+$query = "SELECT * FROM `products` LIMIT 0 , 20";
+
+$comments = mysqli_query($con,$query);
+
+//echo "[";
+$products = array();
+
+while($row = mysqli_fetch_array($comments, MYSQL_ASSOC))
+{
+  $title = $row['title'];
+  $price = $row['price'];
+  $img_url = $row['img_url'];
+  $category = $row['category'];
+  
+  $arrayAdd =  array('title' => $title,'price' => $price,'img_url' => $img_url,'category' => $category);
+  array_push($products, $arrayAdd);
+}
+
+echo json_encode($products);
+mysqli_close($con);
+
+?>
--- Task 2/app/model.js
+++ Task 2/app/model.js
@@ -1,15 +1,12 @@
 (function(){
 	var app = angular.module("Myapp",[]);
-
-	
-
 	app.controller('getMenu',function($scope){
 		$scope.menus = window.menus;
 	});
 
-	app.controller('gallery',function($log , $scope, getProducts){
+	app.controller('gallery',function($log , $scope, Products){
 		
-	   	var promise = getProducts.getproducts();
+	   	var promise = Products.firstRun();
 		
 		promise.then(
           function(response) { 
@@ -20,36 +17,57 @@
           });
 	});
 
-	app.controller('getCategorys',function($scope,getProducts){
-		var promise = getProducts.getproducts();
-		$scope.categorys = ['all'];
-
+	app.controller('getCategorys',function($scope,Products){
+		var promise = Products.firstRun();
+		 $scope.categorys = ['all'];
+		//
 		promise.then(
           function(response) { 
               $scope.products = response.data;
             angular.forEach($scope.products,function(product){
+
 				if($scope.categorys.indexOf(product.category) == -1){
 					$scope.categorys.push(product.category);
 				}
 			});
+           
           },
           function(errorResponse) {
               $log.error('failure loading movie', errorResponse);
           });
-
-		$scope.cuurrProducts = function(){
-			$scope.opCHANGE = $scope.myselect;
-		}
+		debugger
+		$scope.onChange = function(category){
+				debugger
+				firstSelect = category;
+				console.log(category+" and  "+x);
+			}
 	});
 
-	app.factory('getProducts',function($http){
+	app.service('Products',function($http){
 		var products = this;
 		var currentProducts = this;
+
+		function getproducts(){
+
+			$http.get('nukes/nukes.json')
+            	.success(function(response) {
+                	currentProducts = response.data;
+            });
+            	debugger
+            products = currentProducts;
+           return currentProducts;
+		}
+
+		function setproducts( data ){
+			debugger
+			currentProducts = data;
+		}
+
 	   	return{
-	   		getproducts: function(){
-	   				return $http.get('app/data/products.php');
-	   			}
-	   	};
+	   		firstRun : function(){
+	   			return $http.get('app/data/getproduct.php');
+	   	}
+	   }
 	});
 
 })();
